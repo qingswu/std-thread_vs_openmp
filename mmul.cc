7281d3cc6c956038f63031fdeafebad97bdf1593
Joshua Bradt <bradt@nscl.msu.edu>
2016-05-03 16:25:46 -0400

    Updated ThreadPool. Added LFThreadPool to mmul. Re-added dummy (again).

--- mmul.cc
+++ mmul.cc
@@ -1,4 +1,6 @@
 #include <ThreadPool.hh>
+#include <LockfreeThreadPool.hh>
+#include <ThreadPoolBase.hh>
 #include <omp.h>
 #include <iostream>
 #include <vector>
@@ -65,7 +67,7 @@ void ompMatrixMultiply(Matrix& matA, Matrix& matB, Matrix& out)
 
 // This is the main function doing the ThreadPool-based multiplication. I pass the pool
 // into the function to avoid re-spawning the threads each time.
-void tpMatrixMultiply(ThreadPool& tp, Matrix& matA, Matrix& matB, Matrix& out)
+void tpMatrixMultiply(ThreadPoolBase& tp, Matrix& matA, Matrix& matB, Matrix& out)
 {
     // I don't believe capture-by-reference will cause dangling references here
     // since all work should be finished before this function returns.
@@ -147,7 +149,7 @@ void clearMatrices(Matrix& matA, Matrix& matB, Matrix& matC)
 
 // Unfortunately, since `timeMult` above calls `reset` with the same args as `func`,
 // we need this overload for the ThreadPool case.
-void tpClearMatrices(ThreadPool& tp, Matrix& matA, Matrix& matB, Matrix& matC)
+void tpClearMatrices(ThreadPoolBase& tp, Matrix& matA, Matrix& matB, Matrix& matC)
 {
     clearMatrices(matA, matB, matC);
 }
@@ -179,8 +181,8 @@ int main(const int argc, const char** argv)
     std::tie(minTime, meanTime, maxTime) =
         timeMult(ompMatrixMultiply, clearMatrices, numReps, matA, matB, matC);
 
-    printf("            %10s  %10s  %10s\n", "Min", "Mean", "Max");
-    printf("    OpenMP: %10.4e  %10.4e  %10.4e\n", minTime, meanTime, maxTime);
+    printf("              %10s  %10s  %10s\n", "Min", "Mean", "Max");
+    printf("      OpenMP: %10.4e  %10.4e  %10.4e\n", minTime, meanTime, maxTime);
 
     // Use a block to limit scope of thread pool
     {
@@ -189,7 +191,16 @@ int main(const int argc, const char** argv)
         std::tie(minTime, meanTime, maxTime) =
             timeMult(tpMatrixMultiply, tpClearMatrices, numReps, tp, matA, matB, matC);
 
-        printf("ThreadPool: %10.4e  %10.4e  %10.4e\n", minTime, meanTime, maxTime);
+        printf("  ThreadPool: %10.4e  %10.4e  %10.4e\n", minTime, meanTime, maxTime);
+    }
+
+    {
+        LockfreeThreadPool lftp (numThreads);
+
+        std::tie(minTime, meanTime, maxTime) =
+            timeMult(tpMatrixMultiply, tpClearMatrices, numReps, lftp, matA, matB, matC);
+
+        printf("LFThreadPool: %10.4e  %10.4e  %10.4e\n", minTime, meanTime, maxTime);
     }
 
     return 0;
